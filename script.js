2b31db705b41d70a6854e5df055ad455c3a8f2a0
SanLi <106612077+sanli11@users.noreply.github.com>
2023-11-12 04:11:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const floatingButton = document.querySelector("#floating-button");
+
+const facebook = document.querySelector("#facebook");
+const twitter = document.querySelector("#twitter");
+const instagram = document.querySelector("#instagram");
+
+const allServices = document.querySelector("#view-services");
+const allProjects1 = document.querySelector("#view-projects");
+const allProjects2 = document.querySelector("#all-projects");
+
+const address = document.querySelector("#address");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+
+const submit = document.querySelector("#submit");
+
+floatingButton.addEventListener("click", () => {
+  const name = document.querySelector("#name");
+  name.focus();
+});
+
+facebook.addEventListener("click", () => {
+  window.open("https://www.facebook.com", "_blank");
+});
+
+twitter.addEventListener("click", () => {
+  window.open("https://www.twitter.com", "_blank");
+});
+
+instagram.addEventListener("click", () => {
+  window.open("https://www.instagram.com", "_blank");
+});
+
+allServices.addEventListener("click", () => {
+  window.location.replace("services.html");
+});
+
+allProjects1.addEventListener("click", () => {
+  window.location.replace("projects.html");
+});
+
+allProjects2.addEventListener("click", () => {
+  window.location.replace("projects.html");
+});
+
+address.addEventListener("click", () => {
+  window.open("https://maps.app.goo.gl/5m1g4FdhL5N5Hjdr6", "_blank");
+});
+
+email.addEventListener("click", () => {
+  window.open("mailto:info@optimum-remodeling.com", "_blank");
+});
+
+phone.addEventListener("click", () => {
+  window.open("tel:+1-888-405-8908", "_blank");
+});
+
+submit.addEventListener("click", () => {
+  const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const phone = document.querySelector("#phone");
+  const message = document.querySelector("#message");
+
+  if (name.validity.valueMissing) {
+    name.setCustomValidity("Please enter your name");
+  } else {
+    name.setCustomValidity("");
+  }
+
+  if (email.validity.valueMissing) {
+    email.setCustomValidity("Please enter your email");
+  } else if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please enter a valid email");
+  } else {
+    email.setCustomValidity("");
+  }
+
+  if (phone.validity.valueMissing) {
+    phone.setCustomValidity("Please enter your phone number");
+  } else {
+    phone.setCustomValidity("");
+  }
+
+  if (message.validity.valueMissing) {
+    message.setCustomValidity("Please enter your message");
+  } else {
+    message.setCustomValidity("");
+  }
+});
